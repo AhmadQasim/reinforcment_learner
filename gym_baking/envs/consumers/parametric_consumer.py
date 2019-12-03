379b939455465b46a2729349681f3987ba381714
ge73ces <karacabey@mobile-software.de>
2019-12-03 22:10:54 +0100

    minor changes

--- gym_baking/envs/consumers/parametric_consumer.py
+++ gym_baking/envs/consumers/parametric_consumer.py
@@ -1,6 +1,19 @@
 import numpy as np
 from gym_baking.envs.consumers.base_consumer import BaseConsumer
 
+
+def sample_from_multinomial(probs):
+    return np.random.multinomial(1, probs)
+
+
+def sample_beta_prior(weight_vector):
+    return np.random.dirichlet(weight_vector)
+
+
+def sample_from_poisson(mean):
+    return np.random.poisson(mean)
+
+
 class CategoricalConsumerModel(BaseConsumer):
     def __init__(self, config):
         """there should be a list called "weight_list" for the weights of the categories in yaml file
@@ -8,23 +21,17 @@ class CategoricalConsumerModel(BaseConsumer):
         super().__init__(config)
         self.weight_vector = config["weight_list"]
 
-    def _sampleBetaPrior(self, weight_vector):
-        return np.random.dirichlet(weight_vector)
-
-    def _sampleFromMultinomial(self, probs):
-        return np.random.multinomial(1, probs)
-
     def make_orders(self, inventory_products, order_queue, timestep):
-        assertMessage = "Alpha vector size \
+        assert len(self.weight_vector) == len(self.products) + 1, "Alpha vector size \
                        should be 1 bigger than the products size"
-        assert len(self.weight_vector) == len(self.products) + 1, assertMessage
-        beta_prior = self._sampleBetaPrior(self.weight_vector)
-        sampled_category = np.asscalar(self._sampleFromMultinomial(beta_prior).nonzero()[0])
+        beta_prior = sample_beta_prior(self.weight_vector)
+        sampled_category = np.asscalar(sample_from_multinomial(beta_prior).nonzero()[0])
 
         if sampled_category == len(self.products):
-            return 0, []
+            return 0, None
 
-        return 1, sampled_category
+        return 1, [sampled_category]
+
 
 class PoissonConsumerModel(BaseConsumer):
     def __init__(self, config):
@@ -35,8 +42,8 @@ class PoissonConsumerModel(BaseConsumer):
             counts: [50,0,10,10]
         - if it is assumed to be ordered 200 for the whole episode
             counts: [200]
-        as we add new element to the list it will produce more partitions
-        with the values set for the partition as the estimated number of products for that
+        as we add new element to these lists, it will produce more partitions
+        with considering the values as the estimated number of products for that
         partition
         """
         super().__init__(config)
@@ -49,20 +56,18 @@ class PoissonConsumerModel(BaseConsumer):
         lambda_vals = []
         for ind, lambdas in enumerate(self.lambdas_list):
             lambda_vals.append(next(val for index, val in enumerate(lambdas)
-                   if (index+1) * (self.maximum_time_steps/self.numbers_of_dilation[ind]) > timestep))
+                                    if
+                                    (index + 1) * (self.maximum_time_steps / self.numbers_of_dilation[ind]) > timestep))
         return lambda_vals
-
-    def _sampleFromPoisson(self, mean):
-        return np.random.poisson(mean)
 
     def _get_lambdas_list(self, counts_list):
         lambdas_list = []
         for ind, counts in enumerate(counts_list):
-            lambdas_list.append([count*self.numbers_of_dilation[ind]/self.maximum_time_steps for count in counts])
+            lambdas_list.append([count * self.numbers_of_dilation[ind] / self.maximum_time_steps for count in counts])
         return lambdas_list
 
     def make_orders(self, inventory_products, order_queue, timestep):
-        poisson_outcomes = [self._sampleFromPoisson(i) for i in self._lambdas_for_timestep(timestep)]
+        poisson_outcomes = [sample_from_poisson(i) for i in self._lambdas_for_timestep(timestep)]
         number_of_items = np.sum(poisson_outcomes)
         items = [i for i, item_count in enumerate(poisson_outcomes) for _ in range(item_count)]
         return number_of_items, items
