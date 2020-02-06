0906d009f2195b13f98b64358a82aaa71c94d55e
ge73ces <karacabey@mobile-software.de>
2020-02-06 15:51:30 +0100

    Demand models

---
+++ demand_models/ar_demand_predictor.py
@@ -0,0 +1,47 @@
+import os
+os.chdir("..")
+from demand_models.ar import AutoRegression
+
+
+class AutoRegressiveDemandPredictor:
+    def __init__(self,
+                 config_path,
+                 steps,
+                 days,
+                 bins_size,
+                 model_path,
+                 load_model=False):
+
+        """
+        :param config_path: inventory.yaml file path
+        :param steps: episode_max_steps in the inventory.yaml file
+        :param days: number of days to sample for poisson for training
+        :param bins_size: the bin size i.e. number of steps in each bin
+        :param model_path: the path where the model is saved and loaded from
+        :param load_model: load already saved model
+        """
+
+        self.model_path = model_path
+
+        if load_model:
+            self.ar = AutoRegression(model_path, config_path, steps, days, bins_size)
+            self.ar.load_models()
+        else:
+            self.ar = AutoRegression(model_path, config_path, steps, days, bins_size)
+            self.ar.sample_data()
+            self.ar.prepare_data()
+            self.ar.train_ar()
+            self.ar.save_models()
+
+
+    def predict(self, curr_data, pred_steps, item):
+        """
+        :param curr_data: the new data received
+        :param pred_steps: number of predictions to be returned
+        :param item: the product ID of curr_data and prediction data, according to inventory.yaml
+        :return: predictions, numpy array
+        """
+        predictions = self.ar.predict_next_n(curr_data=curr_data, pred_steps=pred_steps, item=item)
+        self.ar.save_models()
+
+        return predictions
